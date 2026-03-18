b56fadb365c90e0a0ad3f586eed9fc88b37e5559
Mikki667 <m_kirs@fanshaweonline.ca>
2026-03-18 19:21:22 -0400

    Songwriter Trait Update
    In this commit I added a SongwriterTrait and used it in Paul McCartney and John Lennons' classes so they can both have a writeSong method.

--- classes/JohnLennon.php
+++ classes/JohnLennon.php
@@ -1,11 +1,13 @@
 <?php
 
 require_once 'Guitarist.php';
+require_once __DIR__ . '/../traits/SongwriterTrait.php';
 
 // John is guitarist and songwriter
 
 class JohnLennon extends Guitarist
 {
+    use SongwriterTrait;
     public function introduce()
     {
         // John's intro message
---
+++ traits/SongwriterTrait.php
@@ -0,0 +1,12 @@
+<?php
+
+// trait for members who write songs
+
+trait SongwriterTrait
+{
+    public function writeSong()
+    {
+        // songwriting message
+        echo "<p>Finding inspiration for a new song... Thinking of love and peace.</p>";
+    }
+}
